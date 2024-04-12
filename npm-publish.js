523d9135b73695f2557c9aae9d8d25cf3192f5f8
xiongshuo.xs <xiongshuo.xs@alibaba-inc.com>
2024-04-12 16:52:17 +0800

    feat: npm run pub

--- npm-publish.js
+++ npm-publish.js
@@ -2,6 +2,4 @@ const fs = require('fs');
 const origin = './dist/main.js';
 const target = './lib/main.js';
 
-fs.rename(origin, target, (err) => {
-    if (err) throw err;
-});
+fs.renameSync(origin, target);
